43dcad3db13b9e885a133faa2bb34aa6d66f5c06
Evan <21ewng@gmail.com>
2023-10-13 03:50:16 -0700

    nav

--- website/src/App.js
+++ website/src/App.js
@@ -1,55 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './Contact';
+import Signup from './Signup';
 
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState(''); // State for the message
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleLogin = () => {
-    // You can add your login logic here
-    if (username === 'yourUsername' && password === 'yourPassword') {
-      setMessage('Login successful');
-    } else {
-      setMessage('Login failed');
-    }
-  };
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className = "top-bar">
-          <div className="login-inputs">
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <button className="login-button" onClick={handleLogin}>
-            Login
-          </button>
-          <div className="small-text">{message}</div> {/* Display the message */}
-         
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <a href="/">Home</a>
+          <a href="/Signup">Sign up</a>
+          <a href="/Contact">Contact</a>
+        </header>
+
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Signup" element={<Signup/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
