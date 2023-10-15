00ee2330da3f03868e89ff9e1f4385d40c6a50f6
Cazazazoo <zhuconnie123@csu.fullerton.edu>
2023-10-14 19:56:54 -0700

    Created New Poll Page CZ 10/14/23

--- website/src/index.js
+++ website/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as ServiceWorker from "./serviceWorker";
-// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,15 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App/>
-//   </BrowserRouter>
-//   document.getElementById('root'));
-// );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// ServiceWorker.unregister();
+
