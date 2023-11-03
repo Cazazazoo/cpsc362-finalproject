1237bde21dbd7abcbdf215253e808ba08d84e7e7
Cazazazoo <zhuconnie123@csu.fullerton.edu>
2023-11-02 20:25:42 -0700

    Add and Remove Options Created Connie Zhu 11/02/23

--- website/src/newpoll.js
+++ website/src/newpoll.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useEffect } from 'react';
-import './newpoll.css';
-
-
+import { useState } from 'react';
+import Axios from 'axios';
+import './Newpoll.css';
 
 function NewPoll () {
     // Function to set the page title
@@ -13,26 +13,75 @@ function NewPoll () {
       setPageTitle("New Poll");
     }, []);
 
-    const handleAddOption = () => {
-      const form = document.getElementById("createNewPoll");
-      const newInput = document.createElement("input");
-      newInput.type = "text";
-      newInput.className = "poll-input";
-      newInput.name = `option${form.elements.length - 2}`;
-      newInput.autofocus = true;
-      newInput.placeholder = `Option ${form.elements.length - 2}`;
-      newInput.required = true;
-      form.insertBefore(newInput, document.querySelector(".add-option-button"));
+    // Keep track of the number of options the poll being created has, default of 2
+    const [optionCount, setOptionCount] = useState(2);
+
+    // Add options to poll
+    const handleAddOption = () => { 
+      // Maximum of 6 options     
+      if (optionCount < 6) {
+        setOptionCount(optionCount + 1);
+      } else {
+        alert("You've reached the maximum number of options (6).");
+      }
     };
 
+    // Remove options from poll
+    const handleRemoveOption = (e) => {
+      // Only allow removal when there are more than 2 options
+      if (optionCount > 2) {
+        setOptionCount(optionCount - 1);
+      }
+    };
+    
+
+    // pollTopic and setTopic, which will hold the current state value and a function to update that state. useState(''): 
+    // This is a call to the useState hook. It initializes the state variable pollTopic with an initial value of an empty string (''). 
+    // The setTopic function is used to update the value of pollTopic.
+    const [pollTopic, setTopic] = useState('');
+    const [pollOptions, setPollOptions] = useState('');
+    const [pollData, setPollData] = useState({});
+
+    // get user input for the topic of the poll
+    const handleTopicChange = (e) => {
+      setTopic(e.target.value);
+    };
+
+    // still figuring this out
+    const handleOptionsChange = (e) => {
+      setPollOptions(e.target.value);
+    };
+  
+    // submit poll
     const handleSubmit = (e) => {
       e.preventDefault();
-      // You can handle form submission here, for example, by sending the data to a server.
+
+      // Prepare the poll data to send to the server
+      const pollData = {
+        topic: pollTopic,
+        options: pollOptions.split(',').map((option) => option.trim()),
+      };
+
+      // Make an HTTP POST request to send the data to the server
+      Axios
+        .post('http://localhost:3000/newPoll', pollData) // Adjust the URL to match your backend route
+        .then((response) => {
+          // Handle the response from the server (e.g., display a success message)
+          alert('Added poll to JSON.');
+          // Clear the form fields if needed
+          setTopic('');
+          setPollOptions('');
+        })
+        .catch((error) => {
+          // Handle any errors (e.g., display an error message)
+          alert('An error occurred while creating the poll.');
+          console.error(error);
+        });
     };
 
     return (
       <div className='new-poll-container'>   
-        <form
+        <form onSubmit={handleSubmit}
           className="poll-form"
           id="createNewPoll"
         >
@@ -42,38 +91,36 @@ function NewPoll () {
               type="text"
               className="poll-form-topic"
               name="topic"
+              value={pollTopic}
+              onChange={handleTopicChange}
               autofocus
               placeholder="Poll Topic"
               required
             ></input>
           </div>
           <br/>
-          <div className="poll-input-group">
+        {/* Render option input fields dynamically based on optionCount */}
+        {Array.from({ length: optionCount }).map((_, index) => (
+          <div className="poll-input-group" key={index}>
             <input
               type="text"
               className="poll-input"
-              name="option1"
-              autofocus
-              placeholder="Option 1"
+              name={`option${index + 1}`}
+              autoFocus
+              placeholder={`Option ${index + 1}`}
               required
-            ></input>
+            />
           </div>
-          <br/>
-          <div className="poll-input-group">
-            <input
-              type="text"
-              className="poll-input"
-              name="option2"
-              autofocus
-              placeholder="Option 2"
-              required
-            ></input>
-          </div>
+          ))}
           <br />
           <button className="add-option-button" type="button" onClick={handleAddOption}>
             Add Option
           </button>
           <br/>
+          <button className="remove-option-button" type="button" onClick={handleRemoveOption}>
+            Remove Option
+          </button>
+          < br/>
           <button className="poll-button" type="submit">
             {/* give it a random 4 number combination */}
             {" "}
@@ -81,6 +128,7 @@ function NewPoll () {
           </button>
           <p class="form__text">
           </p>
+          
         </form>
       </div>
     );
