d71da111e48b5792becbc6075d18cf6083bf75e1
Evan <21ewng@gmail.com>
2023-11-11 21:06:34 -0800

    view answers

--- website/src/viewanswers.js
+++ website/src/viewanswers.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function viewanswers(){
-    const [pollData, setPollData] = useState(null);
     return (
         <div className='view-answer-container'>
           <h1> Poll answers </h1>
