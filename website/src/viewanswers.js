f08e73cb2b64a33edb026aef5175a074f269d8f4
Evan <21ewng@gmail.com>
2023-11-11 21:05:16 -0800

    view answers page

---
+++ website/src/viewanswers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function viewanswers(){
+    const [pollData, setPollData] = useState(null);
+    return (
+        <div className='view-answer-container'>
+          <h1> Poll answers </h1>
+        </div>
+      );
+}
+
+export default viewanswers;
